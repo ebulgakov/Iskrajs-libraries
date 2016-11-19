94be813941e59ded4d4c8e7c0e325a6fdad5d9c7
Evgeniy Bulgakov <zoom81@yandex.ru>
2016-11-19 14:51:41 +0400

    Update HC595.js

--- HC595.js
+++ HC595.js
@@ -1,11 +1,12 @@
 function HC595(pin) {
   this.pin = pin;
-}
+};
 
 HC595.prototype.setInt = function (digist, withZero) {
 
 
-}
+};
+
 exports.connect = function(pin) {
     return new HC595(pin);
 };
